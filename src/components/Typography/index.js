4cec74184048fd943f7b654ab8a711ad9749c241
scriptype <scriptyper@gmail.com>
2019-09-16 00:20:23 +0300

    Prevent passing dark prop to underlying typography elements

--- src/components/Typography/index.js
+++ src/components/Typography/index.js
@@ -1,61 +1,61 @@
 import React from 'react'
 import css from './Typography.module.css'
 
-function H1({ children, ...props }) {
+function H1({ children, dark, ...restProps }) {
   const classNames = [css.h1]
-  if (props.dark) {
+  if (dark) {
     classNames.push(css.dark)
   }
   return (
-    <h1 className={classNames.join(' ')} {...props}>
+    <h1 className={classNames.join(' ')} {...restProps}>
       {children}
     </h1>
   )
 }
 
-function H2({ children, ...props }) {
+function H2({ children, dark, ...restProps }) {
   const classNames = [css.h2]
-  if (props.dark) {
+  if (dark) {
     classNames.push(css.dark)
   }
   return (
-    <h2 className={classNames.join(' ')} {...props}>
+    <h2 className={classNames.join(' ')} {...restProps}>
       {children}
     </h2>
   )
 }
 
-function H3({ children, ...props }) {
+function H3({ children, dark, ...restProps }) {
   const classNames = [css.h3]
-  if (props.dark) {
+  if (dark) {
     classNames.push(css.dark)
   }
   return (
-    <h3 className={classNames.join(' ')} {...props}>
+    <h3 className={classNames.join(' ')} {...restProps}>
       {children}
     </h3>
   )
 }
 
-function H4({ children, ...props }) {
+function H4({ children, dark, ...restProps }) {
   const classNames = [css.h4]
-  if (props.dark) {
+  if (dark) {
     classNames.push(css.dark)
   }
   return (
-    <h4 className={classNames.join(' ')} {...props}>
+    <h4 className={classNames.join(' ')} {...restProps}>
       {children}
     </h4>
   )
 }
 
-function P({ children, ...props }) {
+function P({ children, dark, ...restProps }) {
   const classNames = [css.p]
-  if (props.dark) {
+  if (dark) {
     classNames.push(css.dark)
   }
   return (
-    <p className={classNames.join(' ')} {...props}>
+    <p className={classNames.join(' ')} {...restProps}>
       {children}
     </p>
   )
