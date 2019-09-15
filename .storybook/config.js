be9f0e719bb2f822838d4c11dcab72f03197d184
scriptype <scriptyper@gmail.com>
2019-09-15 17:54:11 +0300

    Add storybook

---
+++ .storybook/config.js
@@ -0,0 +1,8 @@
+import { configure, addDecorator } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y';
+import '@storybook/addon-console'
+
+addDecorator(withA11y)
+
+// automatically import all files ending in *.stories.js
+configure(require.context('../src/stories', true, /\.stories\.js$/), module)
