da22b6444f7d15fd054419a809cd485035c91ad9
scriptype <scriptyper@gmail.com>
2019-09-17 20:53:31 +0300

    Refactor and improve tests of Typography

--- src/components/Typography/Typography.test.js
+++ src/components/Typography/Typography.test.js
@@ -1,33 +1,65 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import createTestHelpers from '../../utils/test-helpers'
+import css from './Typography.module.css'
 import { H1, H2, H3, H4, P } from '.'
 
+const helpers = createTestHelpers(expect, it, document)
+
 it('H1 renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<H1 />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  helpers.rendersWithoutCrashing(H1)
+})
+
+it('H1 has dark mode', () => {
+  helpers.hasDarkMode(H1, `${css.h1}.${css.dark}`)
+})
+
+it('H1 passes through props', () => {
+  helpers.passesThroughProps(H1)
 })
 
 it('H2 renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<H2 />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  helpers.rendersWithoutCrashing(H2)
+})
+
+it('H2 has dark mode', () => {
+  helpers.hasDarkMode(H2, `${css.h2}.${css.dark}`)
+})
+
+it('H2 passes through props', () => {
+  helpers.passesThroughProps(H2)
 })
 
 it('H3 renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<H3 />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  helpers.rendersWithoutCrashing(H3)
+})
+
+it('H3 has dark mode', () => {
+  helpers.hasDarkMode(H3, `${css.h3}.${css.dark}`)
+})
+
+it('H3 passes through props', () => {
+  helpers.passesThroughProps(H3)
 })
 
 it('H4 renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<H4 />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  helpers.rendersWithoutCrashing(H4)
+})
+
+it('H4 has dark mode', () => {
+  helpers.hasDarkMode(H4, `${css.h4}.${css.dark}`)
+})
+
+it('H4 passes through props', () => {
+  helpers.passesThroughProps(H4)
 })
 
 it('P renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<P />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  helpers.rendersWithoutCrashing(P)
+})
+
+it('P has dark mode', () => {
+  helpers.hasDarkMode(P, `${css.p}.${css.dark}`)
+})
+
+it('P passes through props', () => {
+  helpers.passesThroughProps(P)
 })
