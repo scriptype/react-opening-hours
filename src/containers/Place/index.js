1d8095bb69ac64629433344b4fab9accfcdd3ef4
scriptype <scriptyper@gmail.com>
2019-09-19 01:01:10 +0300

    Fix useEffect dependency problem

--- src/containers/Place/index.js
+++ src/containers/Place/index.js
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from 'react'
 import TimeTable from '../../components/TimeTable'
 import PlaceModel from './model'
 
+const model = new PlaceModel()
+
 function Place({ placeId }) {
   const [place, setPlace] = useState({})
-
-  const model = new PlaceModel()
 
   useEffect(() => {
     model.setPlace(placeId).then(() => {
       setPlace(model.parse())
     })
-  }, [])
+  }, [placeId])
 
   console.log({ place })
 
